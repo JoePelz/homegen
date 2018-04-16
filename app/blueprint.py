247305d41ee632aa7eebc23e5b2ab2b30026c530
JoePelz <joe.pelz@gmail.com>
2018-04-15 23:21:29 -0700

    updated render colors

--- app/blueprint.py
+++ app/blueprint.py
@@ -1,4 +1,5 @@
-from app.rooms import BaseRoom
+from typing import List
+from app.rooms import BaseRoom, BaseWall
 from app.transform import Transform2D
 import svgwrite
 # https://svgwrite.readthedocs.io/en/master/
@@ -10,14 +11,31 @@ class Blueprint:
         # scale is how many user units per inch
         self.scale = 1.0  # type: float
         self.dwg = svgwrite.Drawing(self.path)
-        self.dwg.viewbox(-300, -100, 600, 600)
-        self.colors = ['blue', 'green', 'red', 'yellow', 'white', 'black']
+        self.dwg.viewbox(-300, -200, 600, 600)
+        self.colors = ['blue', 'green', 'red', 'yellow', 'white']
         self.color_index = 0
 
     def add_model(self, model: BaseRoom) -> None:
+        if isinstance(model, BaseWall):
+            self.render_wall(model)
+        else:
+            self.render_room(model)
+
+    def export(self) -> None:
+        self.dwg.add(self.dwg.circle((0, 0), r=10, fill='white'))
+        self.dwg.save(pretty=True)
+
+    def render_room(self, model: BaseRoom) -> None:
         points = map(lambda m: m.start, model.edges)
         matrix = Blueprint.transform_to_svg(model.transform)
-        polygon = self.dwg.polygon(points, fill=self.colors[self.color_index % len(self.colors)])
+        polygon = self.dwg.polygon(
+            points,
+            fill=self.colors[self.color_index % len(self.colors)],
+            fill_opacity=0.5,
+            stroke=self.colors[self.color_index % len(self.colors)],
+            stroke_width=2,
+            stroke_linejoin='bevel'
+        )
         self.color_index += 1
         # print("model: {}".format(model.report()))
         # print(" X: {:6} {:6}".format(*matrix[0:2]))
@@ -26,12 +44,21 @@ class Blueprint:
         polygon.matrix(*matrix)
         self.dwg.add(polygon)
 
-    def export(self):
-        self.dwg.add(self.dwg.circle((0, 0), r=10, fill='white'))
-        self.dwg.save(pretty=True)
+    def render_wall(self, model: BaseWall) -> None:
+        points = map(lambda m: m.start, model.edges)
+        matrix = Blueprint.transform_to_svg(model.transform)
+        polygon = self.dwg.polygon(
+            points,
+            fill='none',
+            stroke='black',
+            stroke_width=2,
+            stroke_linejoin='bevel'
+        )
+        polygon.matrix(*matrix)
+        self.dwg.add(polygon)
 
     @staticmethod
-    def transform_to_svg(transform: Transform2D):
+    def transform_to_svg(transform: Transform2D) -> List[float]:
         index_order = 0, 1, 2, 3, 4, 5
         matrix = [transform.values[i] for i in index_order]
         return matrix
